1cc8e17328fadec22611ffe295fdb8c2100c256f
mct2611 <ctzj1026@163.com>
2022-01-20 15:05:01 +0800

    two

--- javase_test02/src/main/java/com/ale/Demo.java
+++ javase_test02/src/main/java/com/ale/Demo.java
@@ -5,6 +5,7 @@ import com.google.inject.Injector;
 
 public class Demo {
     public static void main(String[] args) {
+        //test
         UtilModule utilModule = new UtilModule();
         Injector injector = Guice.createInjector(utilModule);
         Object1 object1 = injector.getInstance(Object1.class);
